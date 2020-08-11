9c7f67fedbbbac102c5255eac3bcd3660ca9d236
thelittlecitizen7 <thelittlecitizen7@gmail.com>
2020-08-11 17:21:38 +0300

    add isCircil list

---
+++ CirclularLinkedListValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace LinkedList
+{
+    public class CirclularLinkedListValidator
+    {
+        public bool IsCircular<T>(LinkedList<T> linkedList)
+        {
+            if (linkedList == null)
+            {
+                return false;
+            }
+            int countNodes = 0;
+            var node = linkedList.First;
+            while (node != null && countNodes <= linkedList.Count) 
+            {
+                node = node.Next;
+                countNodes++;
+            }
+            return (countNodes > linkedList.Count);
+        }
+    }
+}
